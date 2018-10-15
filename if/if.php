0bf32213398e9f5ba64501be5447ae0f1c2f9dc8
AwaludinT <43643847+AwaludinT@users.noreply.github.com>
2018-10-15 13:27:37 +0700

    Add files via upload

---
+++ if/if.php
@@ -0,0 +1,6 @@
+<?php
+$wal=90;
+if ($wal>=60) {
+	echo "Nilai anda $wal, Anda LULUS";
+}
+?>
---
+++ if/if2.php
@@ -0,0 +1,9 @@
+<?php
+$wal=50;
+if ($wal>=60) {
+	echo "Nilai anda $wal, Anda LULUS";
+}
+else{
+	echo "Nilai anda $wal, Anda GAGAL";
+}
+?>
